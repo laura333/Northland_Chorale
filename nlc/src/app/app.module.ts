27a757fdf2a341a3431e39af9e76f20b39aabe19
Laura Baukol <laura.baukol@mac.com>
2017-03-07 15:56:13 -0700

    added content and links to welcome page

--- nlc/src/app/app.module.ts
+++ nlc/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
