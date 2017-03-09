efd39caa8e134af52788e73bd783a0be9a34a60c
Laura Baukol <laura.baukol@mac.com>
2017-03-08 23:00:24 -0700

    updated images

--- src/app/about/images.ts
+++ src/app/about/images.ts
@@ -3,7 +3,7 @@ import { Carousel } from './carousel';
 export var IMAGES: Carousel[] = [
   { "title": "Dance Party! Fall 2016", "url": "../assets/img/Fall2016_2_cropped.jpg" },
   { "title": "It's An Honor To Be Nominated, Spring 2016", "url": "../assets/img/Spring2016_2_cropped.jpg" },
-  { "title": "Big Band Boogie, Fall 2015", "url": "../assets/img/Fall2015.jpg" },
   { "title": "Rockin' to the 50s & 60s, Spring 2014", "url": "../assets/img/Spring2014Show_cropped.jpg" },
-  { "title": "Vegas Then and Now, Fall 2013", "url": "../assets/img/Fall2013_cropped.jpg" }
+  { "title": "Dance Party! Fall 2016", "url": "../assets/img/Fall2016_4.jpg" },
+  { "title": "It's An Honor To Be Nominated, Spring 2016", "url": "../assets/img/Spring2016_3.jpg" }
 ];
